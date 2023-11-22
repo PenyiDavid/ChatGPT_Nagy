e919287adf51283bd95f7245c3ec06ab2f2be6bc
PenyiDavid <penyaskodavid@gmail.com>
2023-11-22 16:52:04 +0100

    Func: weboldal újrakészítése

---
+++ program/cipobolt/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+<div
+    class="hover:bg-gray-600 hover:cursor-pointer hover:text-gray-300 bg-gray-500 bg-opacity-75 shadow-2xl p-4 rounded-lg m-auto lg:w-10/12 md:w-10/12 w-full my-5">
+    <p class="text-3xl text-center font-bold underline">
+        Ez a welcome.blade.php fájlból származik
+    </p>
+</div>
