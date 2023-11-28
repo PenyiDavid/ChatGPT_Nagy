b4a887e7efb383097555369a6d7acbad419eee69
PenyiDavid <penyaskodavid@gmail.com>
2023-11-28 16:19:06 +0100

    Func: Termékek tábla bővítése
    A termékről egy fotónak mező

--- program/cipobolt/database/migrations/2023_11_26_213014_create_termekek_table.php
+++ program/cipobolt/database/migrations/2023_11_26_213014_create_termekek_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('termekek', function (Blueprint $table) {
             $table->id();
             $table->string('termek_neve');
+            $table->string('eleres');
             $table->int('ar');
             $table->string('Meret');
             $table->int('darabszam');
