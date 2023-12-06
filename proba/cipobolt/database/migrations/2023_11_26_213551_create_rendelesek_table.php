379de5b8f4d5f809509a1a6aa05129354a0215d4
PenyiDavid <penyaskodavid@gmail.com>
2023-12-06 16:45:02 +0100

    Func: javítás

---
+++ proba/cipobolt/database/migrations/2023_11_26_213551_create_rendelesek_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rendelesek', function (Blueprint $table) {
+            //$table->engine = 'InnoDB';
+            $table->id();
+            //$table->integer("user_id")->unsigned();
+            //$table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            //$table->index('termek_id'); //->unsigned()->unique();
+            //$table->foreign('termek_id')->references('id')->on('termekek')->onDelete('cascade');
+            //$table->foreign('user_id')->references('user_id')->on('users');
+            //$table->foreign('termek_id')->references('user_id')->on('termekek');
+            $table->foreignId('user_id')->constrained(
+                table: 'users', indexName: 'rendelesek_user_id'
+            )->onDelete('cascade');
+            $table->foreignId('termek_id')->constrained(
+                table: 'termekek', indexName: 'rendelesek_termek_id'
+            )->onDelete('cascade');
+            $table->string('szallitasi_cim');
+            $table->timestamps();
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rendelesek');
+    }
+};
